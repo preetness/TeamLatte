10b00688fc273b39704c54049ac964a7e1f9eec1
Preet Dha <email": null>
2017-07-20 15:53:20 -0700

    Add head partial and create public folder with stylesheet

--- app.js
+++ app.js
@@ -1,18 +1,19 @@
 const express = require('express');
 const app = express();
-
+app.set('view engine', 'ejs');
+app.use(express.static(__dirname + '/public'))
 app.listen(3000);
 
 app.get('/', function(req, res) {
 
-  let options = {
-    root: __dirname,
-    dotfiles: 'deny',
-    headers: {
-      'x-timestamp': Date.now(),
-      'x-sent': true
-    }
-  };
+  // let options = {
+  //   root: __dirname,
+  //   dotfiles: 'deny',
+  //   headers: {
+  //     'x-timestamp': Date.now(),
+  //     'x-sent': true
+  //   }
+  // };
 
-  res.sendFile('index.html', options);
+  res.render('pages/index');
 });
