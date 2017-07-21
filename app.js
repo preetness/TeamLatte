ede5a2afe02a609f69231b4f3cdfaf2bb53c1cea
Kala Wetzler <kala.wetzler@gmail.com>
2017-07-21 11:25:03 -0700

    LOL all the things for stripeification

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+require('dotenv').config()
 const express = require('express');
 const app = express();
 app.set('view engine', 'ejs');
@@ -18,4 +19,40 @@ app.get('/mission', function(req, res){
 
 app.get('/location', function(req, res){
   res.render('pages/location');
+});
+
+// Stripe imports & config
+const keyPublishable = process.env.PUBLISHABLE_KEY;
+const keySecret = process.env.SECRET_KEY;
+
+const stripe = require("stripe")(keySecret);
+// const bodyParser = require('body-parser');
+// app.use(bodyParser.urlencoded({extended: true}));
+// app.use(bodyParser.json());
+const bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
+
+
+// Stripe GET & POST routes
+app.get("/store", (req, res) =>
+  res.render("pages/store", {keyPublishable}));
+
+app.post("/charge", (req, res) => {
+  let amount = 500;
+   console.log(req);
+
+  stripe.customers.create({
+     email: req.body.stripeEmail,
+    source: req.body.stripeToken,
+  })
+  .then(customer =>
+    stripe.charges.create({
+      amount,
+      description: "Sample Charge",
+         currency: "usd",
+         customer: customer.id,
+         metadata: {'product': req.body['product']}
+    }))
+  .then(charge => res.render("pages/charge"));
 });
